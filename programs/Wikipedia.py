fa59850f8ecb06858a8496a1df12ca1e0243ce32
Tristan <tristan.day.uk@gmail.com>
2018-12-19 21:26:48 +0000

    Pre test

--- programs/Wikipedia.py
+++ programs/Wikipedia.py
@@ -1,27 +1,30 @@
 import wikipedia
 import core.log
 log = core.log.name("Wikipedia")
+#Ver pre-finish
+
+def edit_len(ans_len):
+    while True:
+        try:
+            ans_len = int(input("Enter New Result Length: "))
+            break
+        except ValueError:
+            log.warn("Please enter an integer (1-15)")
+    return (ans_len)
 
 def main():
-    print("Basic Wikipedia Query\nComands:\nlength() for setting result length (Sentences)")
-    lang = ("en")
-        while True:
-            try:
-                command = input("> ").lower()
-                if command == "length()":
-                    while ans_len > 0 and ans_len < 15:
-                        try:
-                            while True:
-                                ans_len = int(input("Enter New Result Length: "))
-                        except ValueError:
-                            log.warn("Please enter an integer (1-15)")
+    print("Basic Wikipedia Query\nComands:\nlength() for setting result length (Sentences)\nquit() for quitting the application\n") #Menu
+    lang, ans_len, command = ("en"), 2, ("") #Defult settings
 
-
-
-
-
-'''
-    question = ("Coca-Cola")
-wikipedia.set_lang("en")
-print(wikipedia.summary(question, sentences=2))
-'''
+    while command != "quit()":
+        try:
+            command = input("Enter a Query or Command: > ").lower()
+            if command == "length()":
+                ans_len =edit_len(ans_len)
+            elif command == "quit()":
+                quit()
+            else:
+                wikipedia.set_lang(lang)
+                print(wikipedia.summary(command, sentences=ans_len))
+        except:
+            log.warn("Somthing went wrong, Please try again")
