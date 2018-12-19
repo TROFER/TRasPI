7dc7cb38164239875ad211c56becad03db94d6cf
TWoolhouse <thomasrwoolhouse@gmail.com>
2018-12-19 21:59:02 +0000

    Fixed gitignore added global env vars

--- main.py
+++ main.py
@@ -3,17 +3,18 @@ import core.log
 import core.loader
 import core.config
 import core.execute
+import core.env
 import util.misc
 
 log = core.log.name("Main")
 core.config.load("core", "cfg")
 core.log.level(core.config.cfg["core"]["log level"])
-
-util.misc.cls()
+if "--ssh" in util.misc.argv:
+    core.env.ssh = True
 
 #Example
-core.loader.load("programs.example")
-core.execute.exec(core.loader.modules["example"])
-util.misc.cls()
+# core.loader.load("programs.example")
+# core.execute.exec(core.loader.modules["example"])
+# util.misc.cls()
 
 print("END MAIN")
