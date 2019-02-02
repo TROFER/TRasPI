bd8e89cbf06de208e7d1ece8897423a2a2c9cb6a
Tristan <tristan.day.uk@gmail.com>
2019-02-02 15:32:55 +0000

    Fixed that

--- programs/backlighttesting.py
+++ programs/backlighttesting.py
@@ -10,5 +10,6 @@ while True:
     r = int(input("Enter Red Value MAX: 225 "))
     g = int(input("Enter Green Value MAX: 225 "))
     b = int(input("Enter Blue Value MAX: 225 "))
-    set_backlight(r,g,b)
+    backlight.set_all(r, g, b)
+    backlight.show()
     print("Backlight Set...")
