b472f6acad39c52489e79cf80703593ab18497c1
Tristan <tristan.day.uk@gmail.com>
2019-02-02 15:46:56 +0000

    Linking Ideas

--- programs/BUTTONS_EXAMPLE.py
+++ programs/BUTTONS_EXAMPLE.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python
 
 import signal
+import blk_lte_tstr.py
 from gfxhat import touch
 
 print("""touch.py
-This basic example shows you the channel/event values you can expect
-when touching different buttons.
+This shows how we can use scripts to link the buttons to certain actions
 Press Ctrl+C to exit.
 """)
 
 def handler(channel, event):
-    print("Got {} on channel {}".format(event, channel))
+    print("Button Pressed")
+    blk_lte_tstr.main()
 
 for x in range(6):
     touch.on(x, handler)
--- programs/blk_lte_tstr.py
+++ programs/blk_lte_tstr.py
@@ -5,8 +5,7 @@ def set_backlight(r, g, b):
     backlight.set_all(r, g, b)
     backlight.show()
 '''
-
-while True:
+main():
     r = int(input("Enter Red Value MAX: 225 "))
     g = int(input("Enter Green Value MAX: 225 "))
     b = int(input("Enter Blue Value MAX: 225 "))
