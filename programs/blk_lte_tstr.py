de223d69d564910ddb9217d45937ce6675e6d8ac
Tristan <tristan.day.uk@gmail.com>
2019-02-02 15:55:51 +0000

    Fix

--- programs/blk_lte_tstr.py
+++ programs/blk_lte_tstr.py
@@ -6,9 +6,11 @@ def set_backlight(r, g, b):
     backlight.show()
 '''
 def main_script(r,g,b):
-'''    r = int(input("Enter Red Value MAX: 225 "))
+    '''
+    r = int(input("Enter Red Value MAX: 225 "))
     g = int(input("Enter Green Value MAX: 225 "))
-    b = int(input("Enter Blue Value MAX: 225 "))'''
+    b = int(input("Enter Blue Value MAX: 225 "))
+    '''
     backlight.set_all(r, g, b)
     backlight.show()
     print("Backlight Set...")
