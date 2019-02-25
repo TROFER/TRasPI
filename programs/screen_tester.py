7f2f10cfdfe52a131731bedf73a781c255768134
Tristan <tristan.day.uk@gmail.com>
2019-02-25 21:30:15 +0000

    Fix

--- programs/screen_tester.py
+++ programs/screen_tester.py
@@ -1,6 +1,8 @@
 from gfxhat import lcd
 from random import randint
 
+lcd.clear()
+lcd.show()
 for i in range(0,100):
-    lcd.set_pixel((randint(0,128)),(randint(0,64)),1)
+    lcd.set_pixel((randint(0,127)),(randint(0,63)),1)
     lcd.show()
