22d20634a591005bf8fd0f1ea5e3687c3dfb1e36
Tristan <tristan.day.uk@gmail.com>
2019-02-26 17:15:27 +0000

    Headder

--- programs/display_prog1.py
+++ programs/display_prog1.py
@@ -1,6 +1,8 @@
 from gfxhat import lcd
 lcd.clear()
 lcd.show()
+for header in range(0,128):
+    lcd.set_pixel(header,0,1)
 for line in range(20,63,20):
     for bar in range(0,128):
         lcd.set_pixel(bar,line,1)
