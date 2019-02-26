08ede2fac3f4fef64010646edef9544e89c8f908
Tristan <tristan.day.uk@gmail.com>
2019-02-26 17:14:29 +0000

    Fix

--- programs/display_prog1.py
+++ programs/display_prog1.py
@@ -1,7 +1,7 @@
 from gfxhat import lcd
 lcd.clear()
 lcd.show()
-for line in range(20,60,20):
+for line in range(20,63,20):
     for bar in range(0,128):
         lcd.set_pixel(bar,line,1)
 lcd.show()
