53941fbe5349ba995f6db420aca4345bc2e77270
Tristan <tristan.day.uk@gmail.com>
2019-02-26 18:28:25 +0000

    menu

--- programs/display_prog1.py
+++ programs/display_prog1.py
@@ -8,11 +8,3 @@ for line in range(20,63,20):
         lcd.set_pixel(bar,line,1)
 for footer in range(0,128):
     lcd.set_pixel(header,63,1)
-#################################TEXT########################
-cursorposy = 20
-for cursorposx in range(0,64,4):
-    for letter in range(0,7):
-        for p in range(0,3):
-                lcd.set_pixel(cursorposx,(cursorposy+p),1)
-
-lcd.show()
