8fb547316d49dbc2b770364df815b06c732fd429
Tristan <tristan.day.uk@gmail.com>
2019-02-26 17:16:11 +0000

    Footer now included

--- programs/display_prog1.py
+++ programs/display_prog1.py
@@ -6,4 +6,6 @@ for header in range(0,128):
 for line in range(20,63,20):
     for bar in range(0,128):
         lcd.set_pixel(bar,line,1)
+for footer in range(0,128):
+    lcd.set_pixel(header,63,1)
 lcd.show()
