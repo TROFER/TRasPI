974b36bb02d59b3977dadf781d42cb4cf81123ba
Tristan <tristan.day.uk@gmail.com>
2019-03-01 20:02:23 +0000

    Removing all

--- programs/screen_tester.py
+++
@@ -1,8 +0,0 @@
-from gfxhat import lcd
-from random import randint
-
-lcd.clear()
-lcd.show()
-for i in range(0,8191):
-    lcd.set_pixel((randint(0,127)),(randint(0,63)),1)
-    lcd.show()
--- programs/set_pixel.py
+++
@@ -1,9 +0,0 @@
-from gfxhat import touch, lcd, backlight, fonts
-
-backlight.set_all(255,0, 225)
-backlight.show()
-lcd.clear()
-for x in range(0,64):
-    for y in range(0,128):
-        lcd.set_pixel(y,x,1)
-lcd.show()
