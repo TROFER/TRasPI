4acc4a6589437bfa9e44c65a35bd153ac2a5e661
Tristan <tristan.day.uk@gmail.com>
2019-03-01 08:21:12 +0000

    Signed-off-by: Tristan <tristan.day.uk@gmail.com>

--- programs/clock.py
+++ programs/clock.py
@@ -15,7 +15,7 @@ try:
 
         draw = ImageDraw.Draw(image)
 
-        font = ImageFont.truetype(fonts.Bitbuntu, 13)
+        font = ImageFont.truetype(fonts.clock, 20)
 
         text = strftime("%H:%M", gmtime())
 
