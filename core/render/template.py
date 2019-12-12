e60154489bb4f5e8f19307fb9bbcd49a2b8d4331
TWoolhouse <thomasrwoolhouse@gmail.com>
2019-12-12 17:49:41 +0000

    Trailing Slash

--- core/render/template.py
+++ core/render/template.py
@@ -1,5 +1,5 @@
 from PIL import Image, ImageFont, ImageDraw
 
-PATH = "/home/traspi"
+PATH = "/home/traspi/"
 background = Image.open(PATH+"core/menu.template").convert("P")
 std_font = PATH+"fonts/font.ttf"
