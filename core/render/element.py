60c330a44067b548092d7bb8c5a1ef23abd14b1d
Tristan <tristan.day.uk@gmail.com>
2019-12-14 18:48:46 +0000

    Done Element Text

--- core/render/element.py
+++ core/render/element.py
@@ -2,23 +2,40 @@ from core.render.window import Element, Vector
 from core.render.renderer import Render
 
 import core.render.template
-import PIL
+from PIL import ImageFont
 
 __all__ = ["Text"]
 
 class Text(Element):
 
-    def __init__(self, pos: Vector, text):
+    def __init__(self, pos: Vector, text="Default Text", size=10, colour=1, justify='C'):
         super().__init__(pos)
-        self.text = text
-        self.data = {}
-        self.size = 10
-        self.colour = 0
-        self.font = PIL.ImageFont.truetype(core.render.template.std_font, self.size)
+        self.text, self.size, self.colour, self.justify = text, size, colour, justify
+        set_font(size), get_font_size(), _calc_justify()
+
+    def get_font_size(self):
         self.font_size = self.font.getsize(self.text)
 
+    def set_text(self, text):
+        self.text = text
+        get_font_size(), _calc_justify()
+
+    def set_font(self, size):
+        self.font = ImageFont.truetype(core.render.template.std_font, size)
+
     def render(self):
-        self.Render.draw.text(self.pos, self.text, self.colour, self.font)
+        self.Render.draw.text(self.position, self.text, self.colour, self.font)
+
+    def _calc_justify(self):
+        if self.justify == "C":
+            self.position = (self.pos[0] - (self.font_size[0] // 2), self.pos[1] - (self.font_size[1] // 2))
+        elif self.justify == "L":
+            self.position = (self.pos[0], self.pos[1] - (self.font_size[1] // 2))
+        elif self.justify == "R":
+            self.position = (self.pos[0] - self.font_size[0], self.pos[1] - (self.font_size[1] // 2))
+        else:
+            self.justify = "C"
+            self.position = (self.pos[0] - (self.font_size[0] // 2), self.pos[1] - (self.font_size[1] // 2))
 
 class Rectangle(Element):
 
