c83daaef3779000a65a4a247cdcba3f68f475413
TWoolhouse <thomasrwoolhouse@gmail.com>
2019-12-14 19:41:40 +0000

    Fixed template

--- core/render/template.py
+++ core/render/template.py
@@ -1,5 +1,5 @@
 from PIL import Image, ImageFont, ImageDraw
 from core.sys import PATH
 
-background = Image.open(PATH+"core/menu.template").convert("P")
+background = Image.open(PATH+"core/assets/menu.template").convert("P")
 std_font = PATH+"fonts/font.ttf"
