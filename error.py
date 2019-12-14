a2b88fb8013751ee12efe7d45c4a1c7261568645
Tristan <tristan.day.uk@gmail.com>
2019-12-14 20:11:32 +0000

    Error Menu

---
+++ error.py
@@ -0,0 +1,19 @@
+import core
+
+class Error(core.render.Window):
+
+    template = f"{core.sys.PATH}core/assets/error.template"
+
+    def __init__(self, message):
+        self.message = core.render.element.Text(core.Vector(124, 27), message, size=10, justify="L")
+
+    def render(self):
+        self.message.render()
+
+class Handle(core.render.Handler):
+
+    key = core.render.Button.CENTRE
+    window = Error
+
+    def press(self):
+        self.window.finish()
