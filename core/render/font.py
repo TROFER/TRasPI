b69bd3c794c568c0084d3b33b005be2e3ea5be1f
TWoolhouse <thomasrwoolhouse@gmail.com>
2019-12-14 19:40:22 +0000

    Added Font System

---
+++ core/render/font.py
@@ -0,0 +1,54 @@
+import PIL.ImageFont
+
+class _Font(type):
+
+    _instances = set()
+
+    def __call__(cls, name: str, path: (str, int), size: int=None):
+        if size is None:
+            if isinstance(path, int):
+                size = path
+                for data in cls._instances:
+                    if name in data:
+                        name, path = data
+            else:
+                cls._instances.add((name, path))
+                return None
+        else:
+            cls._instances.add((name, path))
+        return super().__call__(name, path, size)
+
+class Font(metaclass=_Font):
+
+    def __init__(self, name: str, path: (str, int), size: int=None):
+        self._name = name
+        self._path = path
+        self._size = size
+        self._compute_font()
+
+    @property
+    def name(self) -> str:
+        return self._name
+    @property
+    def path(self) -> str:
+        return self._path
+
+    @property
+    def size(self) -> int:
+        return self._size
+
+    @size.setter
+    def size(self, value: int):
+        self._size = value
+        self._compute_font()
+
+    def font(self):
+        return self._font
+
+    def font_size(self, text) -> int:
+        return self._font.getsize(text)
+
+    def _compute_font(self):
+        print(self._path)
+        self._font = PIL.ImageFont.truetype(self._path, self._size)
+        return self._font
