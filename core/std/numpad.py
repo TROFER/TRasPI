839a72a6eb4efaa5ea2fe24fce816e2dbdd594bd
Tristan <tristan.day.uk@gmail.com>
2019-12-14 21:56:08 +0000

    New Assets

---
+++ core/std/numpad.py
@@ -0,0 +1,82 @@
+import core
+
+__all__ = ["Numpad"]
+
+class Numpad(core.render.Window):
+
+    def __init__(self, min, max, default=None):
+        if default is None:
+            default = min
+        self.min, self.max, self.default = min, max, default
+        self.step = 1
+        self.selected_number = core.render.element.TextContainer(core.Vector(64, 32), default)
+        self.left_step = core.render.element.Text(core.Vector(3, 32), f"-{self.step}")
+        self.right_step = core.render.element.Text(core.Vector(125, 32), f"+{self.step}")
+
+    def render(self):
+        self.selected_number.render()
+        self.left_step.render(), self.right_step.render()
+
+    def add(self):
+        if not self.selected_number.value() + self.step > self.max:
+            self.selected_number.value(self.selected_number.value() + self.step)
+        else:
+            self.selected_number.value(self.max)
+
+    def subtract(self):
+        if not self.selected_number.value() - self.step < self.max:
+            self.selected_number.value(self.selected_number - self.step)
+        else:
+            self.selected_number.value(self.min)
+
+    def units_up(self):
+        if not self.step * 10 > self.max:
+            self.step *= 10
+            self.left_step.text(f"-{self.step}")
+            self.right_step.text(f"+{self.step}")
+
+    def units_down(self):
+        if not self.step / 10 < 1:
+            self.step /= 10
+            self.left_step.text(f"-{self.step}")
+            self.right_step.text(f"+{self.step}")
+
+class Handle(core.render.Handler):
+
+    key = core.render.Button.UP
+    window = Numpad
+
+    def press(self):
+        self.window.units_up()
+
+class Handle(core.render.Handler):
+
+    key = core.render.Button.DOWN
+    window = Numpad
+
+    def press(self):
+        self.window.units_down()
+
+class Handle(core.render.Handler):
+
+    key = core.render.Button.LEFT
+    window = Numpad
+
+    def press(self):
+        self.window.subtract()
+
+class Handle(core.render.Handler):
+
+    key = core.render.Button.RIGHT
+    window = Numpad
+
+    def press(self):
+        self.window.add()
+
+class Handle(core.render.Handler):
+
+    key = core.render.Button.CENTRE
+    window = Numpad
+
+    def press(self):
+        self.window.finish(self.window.selected_number.value())
--- temp_test.py
+++ temp_test.py
@@ -1,47 +1,17 @@
 import core
-import time
 
-class Window(core.render.Window):
 
-    def __init__(self):
-        super().__init__()
-        self.text = core.render.element.Text(core.Vector(64, 32), "MAIN WINDOW")
 
-    def render(self):
-        self.text.render()
 
-class SubWindow(core.render.Window):
 
-    def __init__(self):
-        super().__init__()
-        self.text = [
-            core.render.element.Text(core.Vector(64, 32), "ABCDEFGHIJKLM"),
-            core.render.element.Text(core.Vector(64, 32), "NOPQRSTUVWXYZ"),
-        ]
-        self.index = 0
 
-    def render(self):
-        self.text[self.index].render()
-        self.index = (self.index + 1) % len(self.text)
 
-class Handle(core.render.Handler):
 
-    key = core.render.Button.UP
-    window = Window
 
-    def press(self):
-        print("MAIN GOT PRESS EVENT ON", self.window)
-        test = yield SubWindow()
-        print(test)
 
-class Handle(core.render.Handler):
 
-    key = core.render.Button.DOWN
-    window = SubWindow
 
-    def press(self):
-        print("SUB GOT PRESS EVENT ON", self.window)
-        self.window.finish("XD")
 
-w = Window()
-w.show()
+
+
+core.loop()
