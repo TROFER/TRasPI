590fc3283c4a84fda0341a55ecaf2d2b4cff676a
TWoolhouse <thomasrwoolhouse@gmail.com>
2019-12-14 21:56:55 +0000

    added Numpad into std

--- core/std/__init__.py
+++ core/std/__init__.py
@@ -1,4 +1,5 @@
 from core.std.menu import *
 from core.std.popup import *
+from core.std.numpad import *
 
-__all__ = ["Menu", "Info", "Warning", "Error"]
+__all__ = ["Menu", "Info", "Warning", "Error", "Numpad"]
