4d270dfab63a6dfc161086cfc07a74b8df1e900d
TWoolhouse <thomasrwoolhouse@gmail.com>
2019-12-14 22:29:30 +0000

    loop

--- temp_test.py
+++ temp_test.py
@@ -1,17 +1,15 @@
 import core
+import core.sys
+
+print(core.sys.PATH)
+
+info = core.std.Info("Info")
+numpad = core.std.Numpad(2, 10)
+
+menu = core.std.Menu(info=info, num=numpad)
 
 
 
+menu.show()
 
-
-
-
-
-
-
-
-
-
-
-
-core.loop()
+core.render.loop()
