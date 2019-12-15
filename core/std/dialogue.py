c1e93b929a19e04db8a7e3b0809fa0dc717e5783
Tristan <tristan.day.uk@gmail.com>
2019-12-15 18:35:20 +0000

    Fixing

--- core/std/dialogue.py
+++ core/std/dialogue.py
@@ -48,7 +48,7 @@ class Handle(core.render.Handler):
     window = Query
 
     def press(self):
-        self.window.finish(self.selection#NOT RITE)
+        self.window.finish(self.selection) #Not Workin
 
 class Handle(core.render.Handler):
 
