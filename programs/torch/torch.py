ac8a114ac63c92ecbba9ea1b0f6e2161b91e5042
Tristan <tristan.day.uk@gmail.com>
2019-12-15 23:11:01 +0000

    Fixed Syntax Error

--- programs/torch/torch.py
+++ programs/torch/torch.py
@@ -16,7 +16,7 @@ class Torch(core.render.Window):
 
     def change_state(self):
         self.state = not self.state
-        self.template = f"{core.sys.PATH0}programs/torch/{self.state}.template"
+        self.template = f"{core.sys.PATH}programs/torch/{self.state}.template"
         if self.state:
             core.hardware.backlight.fill(225, 225, 225)
         else:
