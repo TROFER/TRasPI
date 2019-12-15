48185fab2fab193baa4d9324cb3663184e2ac659
Tristan <tristan.day.uk@gmail.com>
2019-12-15 16:03:47 +0000

    Experementing

--- temp_test.py
+++ temp_test.py
@@ -5,7 +5,7 @@ print(core.sys.PATH)
 
 info = core.std.Error("Info")
 numpad = core.std.Numpad(2, 8000)
-boolinput = core.std.Query("Test", "Test")
+boolinput = core.std.dialogue.Query("Test", "Test")
 menu = core.std.Menu(info=info, num=numpad, comfirm=boolinput)
 
 
