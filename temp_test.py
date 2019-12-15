fac3ff1d50e3c4dcb1665c26aee66d27be04339f
Tristan <tristan.day.uk@gmail.com>
2019-12-15 16:00:01 +0000

    Experementing

--- temp_test.py
+++ temp_test.py
@@ -3,9 +3,8 @@ import core.sys
 
 print(core.sys.PATH)
 
-info = core.std.Info("Info")
+info = core.std.Error("Info")
 numpad = core.std.Numpad(2, 8000)
-
 menu = core.std.Menu(info=info, num=numpad)
 
 
