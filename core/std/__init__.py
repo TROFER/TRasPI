0106dc0bbee8817c1a2bb18eaddabfbf39509ea7
Tristan <tristan.day.uk@gmail.com>
2019-12-15 16:05:07 +0000

    Experementing

--- core/std/__init__.py
+++ core/std/__init__.py
@@ -1,5 +1,6 @@
 from core.std.menu import *
 from core.std.popup import *
 from core.std.numpad import *
+from core.std.dialogue import *
 
-__all__ = ["Menu", "Info", "Warning", "Error", "Numpad"]
+__all__ = ["Menu", "Info", "Warning", "Error", "Numpad", "Query"]
