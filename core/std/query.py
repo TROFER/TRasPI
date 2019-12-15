4514eace6d44f6b4aaeabda75da6dabdff2bff0c
Tristan <tristan.day.uk@gmail.com>
2019-12-15 20:27:12 +0000

    Waiting for box wrap option

--- core/std/query.py
+++ core/std/query.py
@@ -7,12 +7,22 @@ class Query(core.render.Window):
     template = f"{core.sys.PATH}core/asset/template/query.template"
 
     def __init__(self, message, title="Query", cancel=False):
-        self.message = core.render.element.Text(core.Vector(64, 16), message) #FONT: 10
+        self.message = core.render.element.Text(core.Vector(64, 16), message)
+        self.title = core.render.element.Text(core.Vector(5, 5), title)
         self.selection = True
-        self.cancel = core.render.element.Text(core.Vector(64, 16), "To Cancel Press 'Return'", size=11) if cancel else None #FONT: 8
+        if cancel:
+            self.bttn_yes = core.render.element.Text(core.Vector(42, 56), "Yes")
+            self.bttn_no = core.render.element.Text(core.Vector(84, 56), "No")
+            self.bttn_cancel = core.render.element.Text(core.Vector(126, 56), "Cancel")
+        else:
+            self.canel = None
+            self.bttn_no = core.render.element.Text(core.Vector(31, 56), "No")
+            self.bttn_cancel = core.render.element.Text(core.Vector(94, 56), "Cancel")
 
     def render(self):
         self.message.render()
+        self.title.render()
+        self.bttn_no.render(), self.bttn_yes.render()
         if self.cancel is not None:
             self.cancel.render()
 
