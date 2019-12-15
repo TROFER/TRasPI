19afbae88d24507ee4ce53c5431d09d229159235
Tristan <tristan.day.uk@gmail.com>
2019-12-15 11:58:02 +0000

    Added Limit to Menu Label Size, Added Indicator Arrows To Menu

--- core/std/menu.py
+++ core/std/menu.py
@@ -7,11 +7,16 @@ class Menu(core.render.Window):
     template = f"{core.sys.PATH}core/asset/template/menu.template"
 
     def __init__(self, **items):
-        self.menu_items = [(core.render.element.Text(core.Vector(3, 32), text, size=10, justify="L", colour=0), func) for text, func in items.items()]
+        self.menu_items = [(core.render.element.Text(core.Vector(3, 32), f'{text[:20]}>', justify="L", colour=0), func) for text, func in items.items()]
+        self.down_arrow, self.up_arrow = core.render.element.Text(core.Vector(64, 48), '\\/'), core.render.element.Text(core.Vector(64, 16), '/\\')
         self.index = 0
 
     def render(self):
         self.menu_items[self.index][0].render()
+        if self.index > 0:
+            self.up_arrow.render()
+        if self.index < len(self.menu_items)-1:
+            self.down_arrow.render()
 
     def up(self):
         if self.index > 0:
