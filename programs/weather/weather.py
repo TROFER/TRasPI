a6b44d54f586ae73a477970912f826858e7fda2e
Tristan <tristan.day.uk@gmail.com>
2019-12-17 17:40:15 +0000

    Fix Text Position

--- programs/weather/weather.py
+++ programs/weather/weather.py
@@ -10,9 +10,9 @@ class Mainwindow(core.render.Window):
         self.API = "&appid=dd440727faee99efb0b572bc6d78e7b3"
         self.URL = "http://api.openweathermap.org/data/2.5/weather?"
         self.location = ["id=2641598", "Newport GB"]
-        self.title = core.render.element.Text(core.Vector(4, 4), f"For {self.location[1]}", colour=0, justify="L")
-        self.header1 = core.render.element.Text(core.Vector(4, 54), "Current Weather:", justify="L")
-        self.header2 = core.render.element.Text(core.Vector(4, 59), "Connected to: Open Weather Map", justify="L")
+        self.title = core.render.element.Text(core.Vector(4, 5), f"For {self.location[1]}", colour=0, justify="L")
+        self.header1 = core.render.element.Text(core.Vector(4, 58), "Current Weather:", justify="L")
+        self.header2 = core.render.element.Text(core.Vector(4, 62), "Connected to: Open Weather Map", justify="L")
         self.get_weather()
 
     def render(self):
@@ -22,11 +22,11 @@ class Mainwindow(core.render.Window):
 
     def get_weather(self):
         self.data = json.load(request.urlopen(self.URL+self.location[0]+self.API))
-        self.tempreture = core.render.element.Text(core.Vector(4, 13), f"Temperature: {round(self.data['main']['temp'] - 273.1, 1)}", justify="L")
-        self.pressure = core.render.element.Text(core.Vector(4, 24), f"Pressure: {self.data['main']['pressure']}", justify="L")
-        self.humidity = core.render.element.Text(core.Vector(4, 33), f"Humidity: {self.data['main']['humidity']}", justify="L")
-        self.wind = core.render.element.Text(core.Vector(4, 42), f"Wind Speed: {self.data['main']['humidity']}", justify="L")
-        self.weather = core.render.element.Text(core.Vector(4, 63), f"{self.data['weather'][0]['description']}", justify="L")
+        self.tempreture = core.render.element.Text(core.Vector(4, 14), f"Temperature: {round(self.data['main']['temp'] - 273.1, 1)}", justify="L")
+        self.pressure = core.render.element.Text(core.Vector(4, 22), f"Pressure: {self.data['main']['pressure']}", justify="L")
+        self.humidity = core.render.element.Text(core.Vector(4, 31), f"Humidity: {self.data['main']['humidity']}", justify="L")
+        self.wind = core.render.element.Text(core.Vector(4, 40), f"Wind Speed: {self.data['main']['humidity']}", justify="L")
+        self.weather = core.render.element.Text(core.Vector(4, 60), f"{self.data['weather'][0]['description']}", justify="L")
 
 class Handle(core.render.Handler):
 
