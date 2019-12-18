3ebde1dc2de4f25ed5c0fc305b15dd5d2fd7c894
Tristan <tristan.day.uk@gmail.com>
2019-12-18 19:58:41 +0000

    Experementing

--- home.py
+++ home.py
@@ -8,10 +8,10 @@ class Mainwindow(core.render.Window):
         self.template = f"{core.sys.PATH}core/resource/template/home.template"
         self.title1 = core.render.element.Text(core.Vector(3, 5), "TRasPi OS", justify="L")
         self.title2 = core.render.element.Text(core.Vector(126, 5), time.strftime('%I:%M%p'), justify="R")
-        self.bttn1 = core.render.element.TextBox(core.Vector(64, 18), "Run Program")
-        self.bttn2 = core.render.element.TextBox(core.Vector(64, 30), "Load Program")
-        self.bttn3 = core.render.element.TextBox(core.Vector(64, 42), "System Settings")
-        self.bttn4 = core.render.element.TextBox(core.Vector(64, 54), "Power Options")
+        self.buttons = [self.bttn1 = core.render.element.TextBox(core.Vector(64, 18), "Run Program"),
+        self.bttn2 = core.render.element.TextBox(core.Vector(64, 30), "Load Program"),
+        self.bttn3 = core.render.element.TextBox(core.Vector(64, 42), "System Settings"),
+        self.bttn4 = core.render.element.TextBox(core.Vector(64, 54), "Power Options")]
         print(self.bttn1.position)
         # Variables
         self.index = 0
@@ -21,7 +21,8 @@ class Mainwindow(core.render.Window):
 
     def render(self):
         self.title1.render(), self.title2.render()
-        self.bttn1.render(), self.bttn2.render(), self.bttn3.render(), self.bttn4.render()
+        for button in self.buttons:
+            button.render()
 
     def up(self):
         if self.index > 0:
