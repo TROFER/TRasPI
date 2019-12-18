80ab13e5d92977398e2c1ff6d6eb3eb29e0107fd
Tristan <tristan.day.uk@gmail.com>
2019-12-18 17:52:51 +0000

    Fixing Button Spacing And Re-add titlebar

--- home.py
+++ home.py
@@ -6,19 +6,18 @@ class Mainwindow(core.render.Window):
     def __init__(self):
         # Elements
         self.template = f"{core.sys.PATH}core/resource/template/home.template"
-        self.title1 = core.render.element.Text(core.Vector(3, 5), "TRasPi OS", justify="L") #Could be part of template
+        self.title1 = core.render.element.Text(core.Vector(3, 5), "TRasPi OS", justify="L")
         self.title2 = core.render.element.Text(core.Vector(126, 5), time.strftime('%I:%M%p'), justify="R")
         self.bttn1 = core.render.element.TextBox(core.Vector(64, 20), "Run Program")
-        self.bttn2 = core.render.element.TextBox(core.Vector(64, 30), "Load Program")
-        self.bttn3 = core.render.element.TextBox(core.Vector(64, 40), "System Settings")
-        self.bttn4 = core.render.element.TextBox(core.Vector(64, 50), "Power Options")
+        self.bttn2 = core.render.element.TextBox(core.Vector(64, 31), "Load Program")
+        self.bttn3 = core.render.element.TextBox(core.Vector(64, 42), "System Settings")
+        self.bttn4 = core.render.element.TextBox(core.Vector(64, 53), "Power Options")
         # Variables
         self.index = 0
         self.functions = {0: "core.loader.run", 1: "core.render.load", 2: "core.sys.configurator", 3: "core.sys.powermenu"} #Needs to be ajusted if name changes
 
     def render(self):
-        #self.title1.render()
-        self.title2.render()
+        self.title1.render(), self.title2.render()
         self.bttn1.render(), self.bttn2.render(), self.bttn3.render(), self.bttn4.render()
 
     def up(self):
