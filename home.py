75b7a9d6b3b9160e9a7f9095690528de996247ea
TROFER <tristan.day.uk@gmail.com>
2019-12-18 13:28:47 +0000

    School Work

--- home.py
+++ home.py
@@ -1,17 +1,53 @@
-# I am the man who arranges the scripts
 import core
 import time
 
 class Mainwindow(core.render.Window):
 
     def __init__(self):
-        # HEADER
+        # Elements
         self.template = f"{core.sys.PATH}core/resource/template/home.template"
-        self.title1 = core.render.element.Text(core.Vector(3, 5), "TRasPi OS", colour=0, justify="L")
-        self.title2 = core.render.element.Text(core.Vector(126, 5), time.strftime('%I:%M'), colour=0, justify="R")
-        # BODY
+        self.title1 = core.render.element.Text(core.Vector(3, 5), "TRasPi OS", colour=0, justify="L") #Could be part of template
+        self.title2 = core.render.element.Text(core.Vector(126, 5), time.strftime('%I:%M %r'), colour=0, justify="R")
         self.bttn1 = core.render.element.Text(core.Vector(64, 10), "Run Program")
         self.bttn2 = core.render.element.Text(core.Vector(64, 20), "Load Program")
         self.bttn3 = core.render.element.Text(core.Vector(64, 30), "System Settings")
         self.bttn4 = core.render.element.Text(core.Vector(64, 40), "Power Options")
-        
+        # Variables
+        self.index = 0
+        self.functions = {0: core.loader.run, 1: core.render.load, 2: core.sys.configurator, 3: core.sys.powermenu} #Needs to be ajusted if name changes
+
+    def up(self):
+        if self.index > 0:
+            self.index -=1 #Needs to reposiiton 
+
+    def down(self):
+        if self.index < len(self.functions):
+            self.index +=1
+
+    def select(self):
+        pass
+
+
+class Handle(core.render.Handler):
+
+    key = core.render.Button.CENTRE
+    window = Mainwindow
+
+    def press(self):
+        self.window.select()
+
+class Handle(core.render.Handler):
+
+    key = core.render.Button.UP
+    window = Mainwindow
+
+    def press(self):
+        self.window.up()
+
+class Handle(core.render.Handler):
+
+    key = core.render.Button.DOWN
+    window = Mainwindow
+
+    def press(self):
+        self.window.down()
--- programs/weather/weather.py
+++ programs/weather/weather.py
@@ -45,30 +45,3 @@ class Handle(core.render.Handler):
         self.window.finish()
 
 main = Mainwindow()
-
-
-
-
-'''
-def api():
-    try:
-        with open("key.key", 'r') as key:
-            return "&appid="+key.read()
-    except IOError:
-        # core.error("Could not open file")
-        pass
-
-
-def main():
-    location = "q=Isle%20of%20Wight%20GB"
-    url = f"http://api.openweathermap.org/data/2.5/weather?{location}&appid=dd440727faee99efb0b572bc6d78e7b3{api()}"
-    data = request.urlopen(url).json()
-    f_data = (f"""Temperature: {(round(data['main']['temp']-273.1, 1))}\nPressure: {data['main']['pressure']}
-Humidity: {data['main']['humidity']}\nWeather: {data['weather'][0]['description']}""")
-    print(f_data)
-    log(f_data)
-
-def log(f_data):
-    with open("log.txt", 'w') as log:
-        log.write(f_data)
-'''
