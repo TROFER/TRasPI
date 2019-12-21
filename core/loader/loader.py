782fd2fb8734fda4324a2aec31094fb87417605d
Tristan <tristan.day.uk@gmail.com>
2019-12-21 19:11:42 +0000

    New Loader *Works

--- core/loader/loader.py
+++ core/loader/loader.py
@@ -1,75 +1,68 @@
 import os
 import core
-import core.sys
 
 core.asset.Template("home", path="core/resource/template/std_window.template")
 core.asset.Image("pyscript", path="core/resource/icon/pyfile.icon")
 core.asset.Image("folder", path="core/resource/icon/folder.icon")
 
+class Item:
+
+    def __init__(self, name: str, image: str):
+        self.icon = core.element.Image(core.Vector(0, 0), core.asset.Image(image))
+        self.label = core.element.Text(core.Vector(0, 0), name, justify="L")
+
+    def select(self):
+        pass
+ 
+    def render(self, index):
+        self.icon.pos = core.Vector(2, 15 + 10 * index)
+        self.icon.render()
+        self.label.pos = core.Vector(12, 20 + 10 * index)
+        self.label._calc_justify()
+        self.label.render()
+
+class FolderItem(Item):
+
+    def __init__(self, name, path):
+        super().__init__(name, "folder")
+        self.path = path
+
+    def select(self):
+        return ProgramMenu
+
+class ProgramItem(Item):
+
+    def __init__(self, name):
+        super().__init__(name, "pyscript")
+
+    def select(self):
+        pass
+
 class ProgramMenu(core.render.Window):
 
-    def __init__(self):
-        # Backlight Cfg
-        core.hardware.Backlight.gradient((0, 44, 44, 0, 0))
-        # Load Templates / Icons
-        self.template = core.asset.Template("home")
-        self.py_icon = core.asset.Image("pyscript")
-        self.folder_icon = core.asset.Image("folder")
+    template = core.asset.Template("home")
+
+    def __init__(self, path="programs"):
+        self.index = 0
+        # Backlight
+        core.hardware.Backlight.gradient((240, 180, 240, 180, 240))
         # Index /programs
         self.contents = []
-        self.index = 0
-        for item in os.listdir(f"{core.sys.PATH}programs"):
-            if item[len(item)-3:] == ".py":
-                self.contents.append((item, self.py_icon))
-            elif "." in item:
-                pass
+        for item in os.listdir(f"{core.sys.PATH}{path}"):
+            if "main.py" in os.listdir(f"{core.sys.PATH}{path}/{item}"):
+                self.contents.append(ProgramItem(item))
             else:
-                self.contents.append((item, self.folder_icon))
-        self.contents.insert(0, ("return", "RETURN"))
-        while len(self.contents) % 4 != 0:
-            self.contents.append(("", ""))
+                self.contents.append(FolderItem(item, path))
         # Elements
-        self.dynamic_elements()
         self.title1 = core.render.element.Text(core.Vector(3, 5), "Programs", justify="L")
-
-    def dynamic_elements(self):
-        self.table = [
-        core.render.element.Text(core.Vector(12, 20), (self.contents[self.index][0]).capitalize(), justify="L"),
-        core.render.element.Text(core.Vector(12, 30), (self.contents[self.index + 1][0]).capitalize(), justify="L"),
-        core.render.element.Text(core.Vector(12, 40), (self.contents[self.index + 2][0]).capitalize(), justify="L"),
-        core.render.element.Text(core.Vector(12, 50), (self.contents[self.index + 3][0]).capitalize(), justify="L")]
-        self.cursor = core.render.element.Text(core.Vector(12 + self.table[self.index]._font_size()[0] + 3, self.table[self.index].pos[1]), "<", justify="L")
-        self.icons = [
-        core.render.element.Image(self.contents[self.index][1], core.Vector(1, 20)),
-        core.render.element.Image(self.contents[self.index + 1][1], core.Vector(1, 30)),
-        core.render.element.Image(self.contents[self.index + 2][1], core.Vector(1, 40)),
-        core.render.element.Image(self.contents[self.index + 3][1], core.Vector(1, 50))
-        ]
+        self.cursor = core.render.element.Text(core.Vector(12 + self.contents[self.index].label._font_size()[0] + 3, self.contents[self.index].label.pos[1]), "<", justify="L")
 
     def render(self):
-        self.dynamic_elements()
-        for element in self.table:
-            element.render()
-        #for icon in self.icons: #While waiting for workaround
-            #icon.render()
-
-    def up(self):
-        if self.index + 1 < len(self.contents) - 1:
-            self.index += 1
-
-    def down(self):
-        if self.index - 1 > 0:
-            self.index -= 1
-
-    @core.render.Window.focus
-    def select(self):
-        if ".py" in self.contents[self.index][0]:
-            command = self.content[self.index][0]
-        else:
-            pass
-            # Needs to open a submenu with items from the folder
-
-        res = yield command
+        self.title1.render()
+        self.cursor.render()
+        for index in range(min(4, len(self.contents))):
+            item = self.contents[self.index + index]
+            item.render(index)
 
 class Handle(core.render.Handler):
 
@@ -98,7 +91,7 @@ class Handle(core.render.Handler):
 class Handle(core.render.Handler):
 
     key = core.render.Button.RIGHT
-    window = Mainwindow
+    window = ProgramMenu
 
     def press(self):
         pass
