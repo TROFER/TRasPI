8284b08ef1465a37bc96a875576600659b92045b
Tristan <tristan.day.uk@gmail.com>
2019-12-23 21:11:34 +0000

    Took the "Training Wheels" off power.py

--- core/hardware/power.py
+++ core/hardware/power.py
@@ -2,14 +2,12 @@ class Power:
 
     @classmethod
     def halt(cls):
-        print("WOULD HALT")
-        # core.render.renderer.close()
-        # core.Hardware.Display.clear()
-        # os.system("halt")
+        core.render.renderer.close()
+        core.Hardware.Display.clear()
+        os.system("halt")
 
     @classmethod
     def restart(cls):
-        print("WOULD REBOOT")
-        # core.render.renderer.close()
-        # core.Hardware.Display.clear()
-        # os.system("reboot")
+        core.render.renderer.close()
+        core.Hardware.Display.clear()
+        os.system("reboot")
