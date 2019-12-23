fadd39ea34696dfeb272eb5b2314d1e4aa1970a0
Tristan <tristan.day.uk@gmail.com>
2019-12-23 10:28:59 +0000

    Removed requied argument self

--- core/hardware/power.py
+++ core/hardware/power.py
@@ -2,10 +2,10 @@ from core.render.single import Singleton
 
 class PowerControls(metaclass=Singleton):
 
-    def halt(self):
+    def halt():
         print("WOULD HALT")
         # os.system("halt")
 
-    def restart(self):
+    def restart():
         print("WOULD REBOOT")
         # os.system("reboot")
