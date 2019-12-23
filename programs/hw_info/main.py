a85961c4e559c33cf5d76c63187e9d5958f3f59d
Tristan <tristan.day.uk@gmail.com>
2019-12-23 17:52:14 +0000

    Added unit indicators

--- programs/hw_info/main.py
+++ programs/hw_info/main.py
@@ -18,7 +18,7 @@ class HardwareWindow(core.render.Window):
 
     def update(self):
         if time.time() - self.time > 1:
-            self.data = gpiozero.CPUTemperature(), psutil.cpu_percent(), psutil.virtual_memory()
+            self.data = f"CPU Temperature: {gpiozero.CPUTemperature().temperature}°C", f"CPU Usage: {psutil.cpu_percent()}%",f"Memory Usage: {psutil.virtual_memory().percent}%"
             print(self.data)
             for index, label in enumerate(self.labels):
                 label.text(self.data[index])
