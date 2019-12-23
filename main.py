72c0da5b775fc5578a81093af35f23851ee1413d
TWoolhouse <thomasrwoolhouse@gmail.com>
2019-12-23 17:23:39 +0000

    Main makes sense now

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ import importlib
 import time
 import os
 
-ATTEMPT = 7
-
 try:
     import core
 except ImportError as e:
@@ -21,15 +19,11 @@ if not isinstance(module.main, core.render.Window):
     msg = "FAIL! {}.main <{} '{}'> is not an instance of 'core.render.Window'!".format(module.__name__, type(module.main).__name__, module.main)
     raise OSError(msg)
 
-count = 0
-while count < ATTEMPT:
-    count += 1
-    try:
-        module.main.show()
-        core.render.loop()
-    except Exception as e:
-        raise OSError from e
-        continue
+try:
+    module.main.show()
+    core.render.loop()
+except Exception as e:
+    raise OSError from e
 
 # time.sleep(5)
 # os.system("halt") # Turn off the device
