837781330919b208b863002aa0ddd6d67c497cab
Tristan <tristan.day.uk@gmail.com>
2019-12-23 10:18:44 +0000

    Updated init properly

--- core/hardware/__init__.py
+++ core/hardware/__init__.py
@@ -1,4 +1,4 @@
 from core.hardware.hardware import Backlight, Button
-from core.hardware.power import Power
+from core.hardware.power import PowerControls
 
 __all__ = ["Backlight", "Button", "PowerControls"]
