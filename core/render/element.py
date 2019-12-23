83bcec7bdc870dc7f02373575112363d4ef65a5e
Tristan <tristan.day.uk@gmail.com>
2019-12-23 13:04:30 +0000

    Test

--- core/render/element.py
+++ core/render/element.py
@@ -123,7 +123,7 @@ class Image(Element):
         super().__init__(pos)
         self.image = image
         print(self.image)
-        print(self.Image._image.size())
+        print(self.image.size())
         if justify:
             self._calc_pos()
 
