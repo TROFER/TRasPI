cbeada994e540c0c593549c1c0b77412a82ef8ce
TROFER <tristan.day.uk@gmail.com>
2019-12-26 15:47:56 +0000

    Trying to get musicplayer to work, this one requires Tom assistance

--- programs/music_player/main.py
+++ programs/music_player/main.py
@@ -3,7 +3,7 @@ import os
 import pygame
 # import audio_metadata
 
-@core.render.Window.focus
+#@core.render.Window.focus
 class PlayerWindow(core.render.Window):
 
     template = core.asset.Template("std::window")
@@ -39,47 +39,6 @@ class PlayerWindow(core.render.Window):
         elif self.select_index == 2: # Change to else once tested
             self.track.stop()
 
-@core.render.Window.focus
-class Track:
-
-    def __init__(self, filename, name, artist=None):
-        self.filename = filename #Full filename eg. .wav, .mp3
-        self.name = name
-        self.artist = artist
-        try:
-            self.track = pygame.mixer.Sound(f"{core.sys.PATH}user/music/{filename}")
-        except pygame.error:
-            yield core.std.Error("Track Load Error")
-
-    def play(self):
-        if PlayerWindow.state = 'PA'
-            pygame.mixer.unpause(), PlayerWindow.state = "PL"
-        elif PlayerWindow.state = 'ST':
-            pygame.mixer.play(self.track), PlayerWindow.state = "PL"
-
-    def pause(self):
-        pygame.mixer.pause(), PlayerWindow.state = "PA"
-
-    def stop(self):
-        pygame.mixer.stop(), PlayerWindow.state = "ST"
-
-class StartScreen(core.std.menu):
-
-    def __init__(self):
-        mixer.init()
-        self.library = []
-        for file in os.listdir(f"{core.sys.PATH}user/music/"):
-            if file.contains(".wav") or file.contains(".mp3"):
-                self.library.append(Track(filename, track[:len(track)-3].capitalise()))
-
-        for track in self.library:
-            elements.append(core.std.Menu.Element(
-                core.element.Text(core.Vector(0, 0), self.track.name, justify="L"),
-                data = track,
-                select = PlayerWindow)
-            super().__init__(*elements, title="Open Track")
-
-
 class Handle(core.render.Handler):
 
     key = core.render.Button.LEFT
@@ -111,5 +70,49 @@ class Handle(core.render.Handler):
 
     def press(self):
         self.window.finish()
+
+@core.render.Window.focus
+class Track:
+
+    def __init__(self, filename, name, artist=None):
+        self.filename = filename #Full filename eg. .wav, .mp3
+        self.name = name
+        self.artist = artist
+        try:
+            self.track = pygame.mixer.Sound(f"{core.sys.PATH}user/music/{filename}")
+        except pygame.error:
+            yield core.std.Error("Track Load Error")
+
+    def play(self):
+        if PlayerWindow.state == 'PA':
+            pygame.mixer.unpause()
+            PlayerWindow.state = "PL"
+        elif PlayerWindow.state == 'ST':
+            self.track.play(self.track)
+            PlayerWindow.state = "PL"
+
+    def pause(self):
+        self.track.pause()
+        PlayerWindow.state = "PA"
+
+    def stop(self):
+        self.track.stop()
+        PlayerWindow.state = "ST"
+
+class StartScreen(core.std.Menu):
+
+    def __init__(self):
+        pygame.mixer.init()
+        self.library = []
+        for file in os.listdir(f"{core.sys.PATH}user/music/"):
+            if ".wav" in file or ".mp3" in file:
+                self.library.append(Track(file, file[:len(file)-3]))
+
+        for track in self.library:
+            elements.append(core.std.Menu.Element(
+                core.element.Text(core.Vector(0, 0), self.track.name, justify="L"),
+                data = track,
+                select = PlayerWindow))
+        super().__init__(*elements, title="Open Track")
 
 main = StartScreen()
