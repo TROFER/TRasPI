7ec000a5735dc1b6848a6bc86ae43f2307819fb9
Tristan <tristan.day.uk@gmail.com>
2019-12-27 16:24:09 +0000

    HW Info is now more polished

--- programs/hw_info/main.py
+++ programs/hw_info/main.py
@@ -10,7 +10,7 @@ class HardwareWindow(core.render.Window):
 
     def __init__(self):
         self.time = time.time()
-        core.hardware.Backlight.fill(255, 255, 255)
+        core.hardware.Backlight.fill(200, 170, 170)
         self.title = core.element.Text(core.Vector(3, 5), "HW_Info", justify="L")
         self.labels = [core.element.Text(core.Vector(3, 15), justify="L"),
         core.element.Text(core.Vector(3, 25), justify="L"),
@@ -20,10 +20,10 @@ class HardwareWindow(core.render.Window):
 
     def update(self):
         if time.time() - self.time > 1:
+            self.ip = [l for l in ([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][:1], [[(s.connect(('8.8.8.8', 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) if l][0][0]
             self.data = (f"CPU Temp: {round(gpiozero.CPUTemperature().temperature, 1)}°C",
              f"CPU Usage: {psutil.cpu_percent()}%", f"Memory Usage: {psutil.virtual_memory().percent}%",
-             f"IP Address: {print([l for l in ([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][:1],
-              [[(s.connect(('8.8.8.8', 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) if l][0][0])}")
+             f"IP: {self.ip}")
             for index, label in enumerate(self.labels):
                 label.text(self.data[index])
             self.time = time.time()
