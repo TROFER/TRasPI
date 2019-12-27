8c353435eca7a8678671b8c3234cb6a0c38dbe3c
Tristan <tristan.day.uk@gmail.com>
2019-12-27 13:47:59 +0000

    Music player no longer crashes when no songs are in the music folder

--- programs/music_player/main.py
+++ programs/music_player/main.py
@@ -117,9 +117,13 @@ class StartScreen(core.std.Menu):
         pygame.mixer.init()
         self.library = []
         elements = []
-        for file in os.listdir(f"{core.sys.PATH}user/music/"):
-            if ".wav" in file or ".ogg" in file:
-                self.library.append(Track(file, file[:len(file)-4]))
+        try:
+            for file in os.listdir(f"{core.sys.PATH}user/music/"):
+                if ".wav" in file or ".ogg" in file:
+                    self.library.append(Track(file, file[:len(file)-4]))
+        except:
+            self.lib_empty()
+            self.finish()
 
         for track in self.library:
             elements.append(core.std.Menu.Element(
@@ -132,5 +136,10 @@ class StartScreen(core.std.Menu):
     def start(self, element, window):
         player = PlayerWindow(element.data)
         yield player
+
+    @core.render.Window.focus
+    def lib_empty(self):
+        yield core.std.Warning("Libary is empty")
+
 
 main = StartScreen()
