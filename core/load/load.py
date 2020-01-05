ed7d0292343dcc26f09bba03cb7de4454edf7613
TWoolhouse <thomasrwoolhouse@gmail.com>
2020-01-05 16:10:45 +0000

    Loader changes sys.path

--- core/load/load.py
+++ core/load/load.py
@@ -1,3 +1,4 @@
+import sys
 from core.sys import PATH
 import core.system.log
 from core.render.window import Window
@@ -7,9 +8,10 @@ loading_count = 0
 @core.render.Window.focus
 def load(program: str, path: str="programs", file: str="main"):
     global loading_count
-    path = "{}{}/{}/main.py".format(PATH, path, program)
+    path = "{}{}/{}/".format(PATH, path, program)
+    file_path = path + "main.py"
     try:
-        spec = importlib.util.spec_from_file_location("module<{}:{}>".format(loading_count, program), path)
+        spec = importlib.util.spec_from_file_location("module<{}:{}>".format(loading_count, program), file_path)
         module = importlib.util.module_from_spec(spec)
         spec.loader.exec_module(module)
         if not hasattr(module, "main"):
@@ -17,9 +19,11 @@ def load(program: str, path: str="programs", file: str="main"):
         if not isinstance(module.main, Window):
             return TypeError("'main' <{}> is not of type <{}>".format(type(module.main).__name__, Window.__name__))
         loading_count += 1
-        return module.main
+        sys.path.insert(0, path)
+        yield module.main
+        sys.path.remove(path)
     except FileNotFoundError:
-        return FileNotFoundError("'{}.py' not in '{}{}'".format(file, path, program))
+        return FileNotFoundError("'{}.py' not in '{}{}'".format(file, file_path, program))
     #except:
     #    print(error)
     #    yield core.std.Error("Load Error")
