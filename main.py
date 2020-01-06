76b2b557f1c97d974939ade8329b885ebd2e15da
TWoolhouse <thomasrwoolhouse@gmail.com>
2020-01-06 22:31:32 +0000

    Main error logging

--- main.py
+++ main.py
@@ -37,4 +37,4 @@ while True:
                 raise core.error.FatalCoreException() from e
             except core.error.FatalCoreException as e:
                 core.system.log.Log.error(e.log())
-                print(e.log())
+                print("MAIN", e.log())
