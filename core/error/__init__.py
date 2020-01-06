d6bcf12eae12d57f19f9461d99a640e0f87743cb
TWoolhouse <thomasrwoolhouse@gmail.com>
2020-01-06 22:06:46 +0000

    Fixed import statement

--- core/error/__init__.py
+++ core/error/__init__.py
@@ -1,3 +1,3 @@
-from core.error import FatalCoreException, WindowError, RenderError, AssetError, HardwareError
+from core.error.base import FatalCoreException, WindowError, RenderError, AssetError, HardwareError
 
 __all__ = ["FatalCoreException", "WindowError", "RenderError", "AssetError", "HardwareError"]
