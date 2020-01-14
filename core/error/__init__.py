1b1a3da81824482956ee17bd5ef28c3d7196c7c9
TWoolhouse <thomasrwoolhouse@gmail.com>
2020-01-14 20:01:20 +0000

    1st error of many

--- core/error/__init__.py
+++ core/error/__init__.py
@@ -1,3 +1,5 @@
 from core.error.base import FatalCoreException, WindowError, RenderError, AssetError, HardwareError, FocusError, EventError, SystemError
-
-__all__ = ["FatalCoreException", "WindowError", "RenderError", "AssetError", "HardwareError", "FocusError", "EventError", "SystemError"]
+from core.error.system import SystemLoadError, SystemLoadModuleError, SystemLoadWindowError
+__all__ = ["FatalCoreException", "WindowError", "RenderError", "AssetError", "HardwareError", "FocusError", "EventError", "SystemError",
+    "SystemLoadError", "SystemLoadModuleError", "SystemLoadWindowError",
+]
