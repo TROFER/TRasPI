d797853132caeb8a732ef3d90497608f1c034204
TROFER <tristan.day.uk@gmail.com>
2020-01-15 09:22:01 +0000

    Removed render.close() from power.py to stop glitching

--- core/hardware/power.py
+++ core/hardware/power.py
@@ -6,12 +6,8 @@ class Power:
 
     @classmethod
     def halt(cls):
-        core.render.close()
-        time.sleep(1)
         os.system("halt")
 
     @classmethod
     def restart(cls):
-        core.render.close()
-        time.sleep(1)
         os.system("reboot")
