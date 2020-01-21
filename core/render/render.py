357f839fd457b52ed007e3f40883404bb75d0abc
TWoolhouse <thomasrwoolhouse@gmail.com>
2020-01-21 21:40:09 +0000

    lookin at da events

--- core/render/render.py
+++ core/render/render.py
@@ -37,6 +37,9 @@ class Render(metaclass=Singleton):
             except Exception as e:
                 raise RenderError("Main Render Loop") from e
         else:
+            print(self.render)
+            print(self.render._render_event.is_set())
+            print(self.render._pause_event.is_set())
             print("OH NO")
             raise core.error.RenderError("TEST") from None
 
