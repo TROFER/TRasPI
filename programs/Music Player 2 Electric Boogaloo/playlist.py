8856fa04c6779713c47b33093e77df4771e3edd5
TROFER <tristan.day.uk@gmail.com>
2020-01-30 16:42:41 +0000

    School Work

--- programs/Music Player 2 Electric Boogaloo/playlist.py
+++ programs/Music Player 2 Electric Boogaloo/playlist.py
@@ -1,8 +1,7 @@
 import core
 import json
 
-
-class StartScreen(core.std.Menu): #Open playlist window
+class StartScreen(core.std.Menu):
 
     def __init__(self):
         self.library = []
@@ -20,19 +19,21 @@ class StartScreen(core.std.Menu): #Open playlist window
         elements = []
         for playlist in self.library:
             elements.append(core.std.Menu.Element(
-                core.element.Text(core.Vector(0, 0), playlist.name, justify="L"),
-                data = track,
-                select = self.start))
+                core.element.Text(core.Vector(
+                    0, 0), playlist.name, justify="L"),
+                data=track,
+                select=self.start))
 
         super().__init__(*elements, title="Open Playlist")
 
     @core.render.Window.focus
     def start(self, element, window):
-        player = player.PlayerWindow(element.data) #Needs to pass the playlist (Type List)
+        player = player.PlayerWindow(element.data)
         yield player
 
     @core.render.Window.focus
     def lib_empty(self):
         yield core.std.Warning("Libary is empty")
+
 
 main = StartScreen()
