bf223645ccc97571651eee203bf2efdb8b55a0fc
Tristan <tristan.day.uk@gmail.com>
2020-02-21 18:41:25 +0000

    Reworked clock

--- programs/Toolbox/Clock/main.py
+++ programs/Toolbox/Clock/main.py
@@ -11,47 +11,52 @@ class Main(core.render.Window):
     def __init__(self):
         self.timer = 0
         self.timeout = 0
-        self.data = self.ldr(default)
-        if self.data is not None:
-            # core.asset.Font("font", self.data["clock"]["font"])
-            core.asset.Template("template", path=self.data["background"])
-            core.hardware.Backlight.gradient([int(val) for val in self.data["backlight"][0]], saturation=int(self.data["backlight"][1]), value=int(self.data["backlight"][2]))
-            self.elements = [(core.element.Text(core.Vector(int(val) for val in element["pos"]), "LDR", element["font"],
-                                          element["size"], element["colour"], element["justify"]), element["format"]) for element in self.data["elements"]]
-                                    
+        self.data = self.load(default)
+        core.asset.Template(
+            "template", path=f"{core.sys.PATH}programs/Toolbox/Clock/package/{default}/template.template")
+        core.hardware.Backlight.gradient([int(val) for val in self.data["backlight"][0]], saturation=int(
+            self.data["backlight"][1]), value=int(self.data["backlight"][2]))
+        self.elements = [(core.element.Text(core.Vector(int(element["pos"][0]), int(element["pos"][1])), text="LDR", font=element["font"], size=int(
+            element["size"]), colour=int(element["colour"]), justify=element["justify"]), element["format"]) for element in self.data["elements"]]
+        print(([int(val) for val in self.data["backlight"][0]], int(
+            self.data["backlight"][1]), int(self.data["backlight"][2])))
 
     def active(self):
-        core.hardware.Backlight.gradient(self.data["backlight"])
+        print(([int(val) for val in self.data["backlight"][0]], int(
+            self.data["backlight"][1]), int(self.data["backlight"][2])))
+        core.hardware.Backlight.gradient([int(val) for val in self.data["backlight"][0]], saturation=int(
+            self.data["backlight"][1]), value=int(self.data["backlight"][2]))
         self.timeout = 0
 
     def inactive(self):
-        core.hardware.Backlight.gradient(self.data["backlight"], value=0.5)
-
-    def ldr(self, name):
-        try:
-            with open(f"{core.sys.PATH}programs/Toolbox/Clock/package/{name}/clock.cfg") as file:
-                return json.load(file)
-        except FileNotFoundError:
-            # yield core.std.Error("Default Missing")
-            print("FileNotFound")
-        except json.JSONDecodeError:
-            print("JSONDecodeError")
-            # yield core.std.Error("Bad JSON format")
-
-# core.asset.Font("font"),
+        core.hardware.Backlight.gradient([int(val) for val in self.data["backlight"][0]], saturation=int(
+            self.data["backlight"][1]), value=0.1)
 
     def render(self):
-        while not time.time() - self.timer > 1:
-            time.sleep(time.time() - self.timer)
         for element in self.elements:
             element[0].text(time.strftime(element[1]))
-            element[1].render()
-        if self.timeout > 60:
+            element[0].render()
+        if time.time() - self.timer > 1 and 60 >= self.timeout:
             self.timeout += 1
-        else:
-            if timeout:
+            self.timer = time.time()
+            if self.timeout == 60:
                 self.inactive()
-        self.timer = time.time()
+        print(self.timeout)
+
+    def load(self, package):
+        try: 
+            with open(f"{core.sys.PATH}programs/Toolbox/Clock/package/{package}/main.cfg", 'r') as file:
+                return json.load(file)
+        except FileNotFoundError:
+            self.errorHandle("No File")
+        except json.JSONDecodeError:
+            self.errorHandle("JSON Error")
+
+    @core.render.Window.focus
+    def errorHandle(self, string):
+        print(string)
+        yield core.std.Error(string)
+        self.finish()
 
 
 class Handle(core.render.Handler):
