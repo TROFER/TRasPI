ab9c4c281ada89ee18174da50b5aafdd97531842
TWoolhouse <thomasrwoolhouse@gmail.com>
2020-04-10 21:57:28 +0100

    Config

--- core/sys/config.py
+++ core/sys/config.py
@@ -1,7 +1,11 @@
 class _MetaConfig(type):
 
     def __new__(cls, name, bases, dct):
-        callbacks = dct["_callback"] if "_callback" in dct else {}
+        if "_callback" in dct:
+            callbacks = dct["_callback"]
+            del dct["_callback"]
+        else:
+            callbacks = {}
         _vars = {}
         for key, var in [(k,v) for k,v in dct.items() if k[0] != "_"]:
             callback = callbacks[key] if key in callbacks else lambda x: None
@@ -11,10 +15,19 @@ class _MetaConfig(type):
         return super().__new__(cls, name, bases, dct)
 
     def __getattribute__(cls, name):
-        return super().__getattribute__("__vars")[name][0]
+        try:
+            return super().__getattribute__("__vars")[name][0]
+        except KeyError:
+            raise AttributeError
 
     def __setattr__(cls, name, value):
-        attr = super().__getattribute__("__vars")[name]
+        try:
+            attr = super().__getattribute__("__vars")[name]
+        except KeyError:
+            if name == "_set_cb_":
+                super().__getattribute__("__vars")[value[0]][1] = value[1]
+                return
+            raise AttributeError
         attr[0] = value
         attr[1](value)
 
@@ -22,8 +35,4 @@ class Config(metaclass=_MetaConfig):
     pass
 
 class SystemConfig(Config):
-    _callback = {
-        "brightness": lambda x: print("CB", x),
-
-    }
-    brightness = 1
+    brightness = 65
