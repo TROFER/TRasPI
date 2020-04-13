12fb5e11719fc1f8d009a6f5523539c61aa18280
Tristan <tristan.day.uk@gmail.com>
2020-04-13 20:01:32 +0100

    Fixed default attributes file

--- core/sys/attributes.py
+++ core/sys/attributes.py
@@ -22,10 +22,10 @@ class Constant(metaclass=MetaConstant):
 
 class SysConstant(Constant):
     width = 128
-    height = 7
+    height = 64
     path = os.path.dirname(os.path.abspath(sys.argv[0])).replace("\\", "/") + "/"
     platform = "NT" if os.name == "nt" else "POSIX"
-    pipeline = "DUMMY" # "GFXHAT" or "DUMMY"
+    pipeline = "GFXHAT" # "GFXHAT" or "DUMMY"
 
 # __all__ = ["Config"]
 
