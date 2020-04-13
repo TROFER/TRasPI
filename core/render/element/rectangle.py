02d9e78936e9ad1e0a3d96e4bd6176cdbdda3c9f
TWoolhouse <thomasrwoolhouse@gmail.com>
2020-04-13 20:45:55 +0100

    fixed rect

--- core/render/element/rectangle.py
+++ core/render/element/rectangle.py
@@ -1,6 +1,8 @@
 from core.vector import Vector
 from core.render.primative import Primative
 
+import PIL.ImageDraw
+
 __all__ = ["Rectangle"]
 
 class Rectangle(Primative):
@@ -14,4 +16,4 @@ class Rectangle(Primative):
         return self.pos1, self.pos2, self.outline, self.fill, self.width
 
     def render(self, image: "PIL.ImageDraw.ImageDraw"):
-        image.rectangle([*self.pos1, *self.pos2], self.fill, self.outline, self.fill)
+        image.rectangle([*self.pos1, *self.pos2], self.fill, self.outline, self.width)
