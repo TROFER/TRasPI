bd09c0092dacc2dc881fde522f4f938c292015ac
TROFER <tristan.day.uk@gmail.com>
2020-05-13 18:53:52 +0100

    Pushing Changes

--- programs/System Information/cpu.py
+++ programs/System Information/cpu.py
@@ -4,8 +4,6 @@ from core import Vector
 from core import Interface
 from core.render.element import Line, Text
 
-def constrain(n, start1, stop1, start2, stop2):
-    return int(((n - start1) / (stop1 - start1)) * (stop2 - start2) + start2)
 
 class Graph:
 
@@ -40,20 +38,21 @@ class Main(core.render.Window, Graph):
             Text(Vector(3, 40), "CPU Speed"),
             Line(Vector(0, 46), Vector(128, 46), width=2)]
         App.interval(self.refresh)
-    
+
     def render(self):
         for element in self.elements:
             Interface.render(element)
-            
 
     def refresh(self):
         self.elements[1:4].text = f"CPU Load: {CPU.load()}%", f"CPU Temp: {CPU.tempreture()}°C", f"CPU Speed: {CPU.cur_speed()}Mhz"
         self.elements[5].pos2 = Vector(constrain(CPU.load, 1, 128, 0, 100), 36)
-        self.elements[7].pos2 = Vector(constrain(CPU.Speed, 1, 128, 0, CPU.max_speed()), 46)
+        self.elements[7].pos2 = Vector(
+            constrain(CPU.Speed, 1, 128, 0, CPU.max_speed()), 46)
+
 
 class Handle(core.input.event.Handle):
 
-    window=Main
+    window = Main
 
     class press:
         async def right(null, window):
