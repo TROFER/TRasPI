ccc9bd0ac93f94cef2b17833608bd30337e13bf0
TROFER <tristan.day.uk@gmail.com>
2020-07-04 10:06:10 +0100

    CPU + Util

--- programs/System Information/main.py
+++ programs/System Information/main.py
@@ -1,6 +1,4 @@
 from app import App
-import os
-import subprocess
 import core
 import cpu
 #import memory
@@ -33,41 +31,6 @@ class Main(core.render.Window):
                 self.finish()
             else:
                 self.index = (self.index + res) % len(self.map)
-
-
-class Hardware:
-
-    class CPU:
-
-        def temperature():
-            return round(gpiozero.CPUTemperature().temperature, 1)
-
-        def load():
-            return psutil.cpu_percent()
-
-        def cur_speed():
-            os.system("cat /sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_cur_freq")
-            return int(subprocess.check_output("cat /sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_cur_freq", shell=True).decode())
-
-        def max_speed():
-            os.system("cat /sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_max_freq")
-            return int(subprocess.check_output("cat /sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_max_freq", shell=True).decode())
-
-    class Memory:
-
-        def load():
-            return int(psutil.virtual_memory().used // 1000000)
-
-        def vmem():
-            return int(psutil.swap_memory().used // 1000000)
-        
-        def total():
-            return int(psutil.virtual_memory().total // 1000000)
-
-
-
-def constrain(n, start1, stop1, start2, stop2):
-    return int(((n - start1) / (stop1 - start1)) * (stop2 - start2) + start2)
 
 App.window = Main
 main = App
