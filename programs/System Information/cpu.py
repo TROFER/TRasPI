ccd024c9c6d29de1c8e8026acce0b85a0906e8e7
Tristan <tristan.day.uk@gmail.com>
2020-09-23 23:45:04 +0100

    Removed old programs/ upgraded sys info

--- programs/System Information/cpu.py
+++ programs/System Information/cpu.py
@@ -33,14 +33,16 @@ class Main(core.render.Window, Graph):
     def __init__(self):
         super().__init__()
         self.elements = [
-            Text(Vector(3, 5), "CPU - System Information", justify='L'),
-            Text(Vector(3, 15), "", justify="L"),
-            Text(Vector(3, 22), "", justify="L"),
-            Text(Vector(3, 29), "", justify="L"),
-            Text(Vector(3, 40), "CPU Load", justify="L"),
-            Line(Vector(0, 53), Vector(128, 53), width=2),
-            Text(Vector(3, 55), "CPU Speed", justify="L"),
-            Line(Vector(0, 62), Vector(128, 62), width=2)]
+            Text(Vector(3, 5), "CPU - System Info..", justify='L'),
+            Line(Vector(0,  10), Vector(128, 10)),
+            Text(Vector(3, 16), "", justify="L"),
+            Text(Vector(3, 23), "", justify="L"),
+            Text(Vector(3, 30), "", justify="L"),
+            Line(Vector(0,  35), Vector(128, 35)),
+            Text(Vector(3, 42), "CPU Load", justify="L"),
+            Line(Vector(3, 47), Vector(128, 47), width=2),
+            Text(Vector(3, 56), "CPU Speed", justify="L"),
+            Line(Vector(3, 61), Vector(128, 61), width=2)]
         self.graph = Graph()
         App.interval(self.refresh)
 
@@ -49,21 +51,15 @@ class Main(core.render.Window, Graph):
             Interface.render(element)
 
     def refresh(self):
-
         for elm, text in zip(self.elements[1:4], (f"CPU Load: {Hardware.CPU.load()}%", f"CPU Temp: {Hardware.CPU.temperature()}°C", f"CPU Speed: {Hardware.CPU.cur_speed()}Mhz")):
             elm.text = text
-        self.elements[5].pos2 = Vector(constrain(Hardware.CPU.load(), 1, 128, 0, 100), 36)
-        self.elements[7].pos2 = Vector(
-            constrain(Hardware.CPU.cur_speed(), 1, 128, 0, Hardware.CPU.max_speed()), 46)
-
-
         self.elements[2].text = f"CPU Load: {Hardware.CPU.load()}%"
         self.elements[3].text = "CPU Temp: {}°C {}".format(Hardware.CPU.temperature(), '/\\' if self.graph.trend() else '\\/')
         self.elements[4].text = f"CPU Speed: {Hardware.CPU.cur_speed() // 1000}Mhz"
         self.elements[7].pos2 = Vector(
-            constrain(Hardware.CPU.load(), 0, 100, 0, 128), 50)
+            constrain(Hardware.CPU.load(), 0, 100, 3, 125), 47)
         self.elements[9].pos2 = Vector(
-            constrain(Hardware.CPU.cur_speed(), 1, Hardware.CPU.max_speed(), 0, 128), 62)
+            constrain(Hardware.CPU.cur_speed(), 1, Hardware.CPU.max_speed(), 3, 125), 61)
         self.graph.plot(Hardware.CPU.temperature())
 
 class Handle(core.input.Handler):
@@ -75,7 +71,9 @@ class Handle(core.input.Handler):
             window.finish(1)
 
         async def left(null, window: Main):
+            pass
             window.finish(-1)
 
         async def centre(null, window: Main):
+            pass
             window.finish()
