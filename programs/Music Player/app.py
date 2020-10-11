db7253c3b7c8e7fcf74db973565ea63a4c34b76e
Tristan <tristan.day.uk@gmail.com>
2020-10-11 16:14:35 +0100

    Music Player 1.3

--- programs/Music Player/app.py
+++ programs/Music Player/app.py
@@ -1,16 +1,14 @@
 import core
-from index import Index
 
 class App(core.type.Application):
     name = "Music Player"
 
     class var(core.type.Config):
-        rescan = True
-        library = []
+        rescan = False
+        directories = [f"{core.sys.const.path}user/music"]
         volume = 75
 
     class const(core.type.Constant):
-        directories = []
         screen_timeout = 30
         colour = core.sys.var.colour
 
@@ -28,11 +26,3 @@ class App(core.type.Application):
 
     def constrain(n, start1, stop1, start2, stop2):
         return ((n - start1) / (stop1 - start1)) * (stop2 - start2) + start2
-
-    def rescan(force=False):
-        if App.var.rescan or force:
-            App.var.library = Index.scan()
-            if App.const.directories:
-                for directory in App.const.directories:
-                    App.var.library = App.var.library + Index.scan(path=directory)
-            App.var.rescan = False
--- programs/Music Player/main.py
+++ programs/Music Player/main.py
@@ -1,23 +1,23 @@
-from app import App
-from window import single
-from index import Index
 import core
+from library import Library
+from app import App
+from window import browser
 
 ############## TRASPI MUSIC PLAYER ###############
 ## Version: 1.1                                 ##
 ## Created By: Tristan Day                      ##
 ## Date: 2020                                   ##
-################################################## 
+##################################################
 
 class Main(core.render.Window):
 
     def __init__(self):
         super().__init__()
-        App.rescan()
+        self.library = Library()
         self._flag = True
         self.index = 0
-        self.map = [single.Main()]
-
+        self.map = [browser.Top(self.library.db, "genre", title="Genres - MP")]
+    
     async def show(self):
         if self._flag:
             self._flag = False
@@ -29,7 +29,6 @@ class Main(core.render.Window):
                 else:
                     self.index = (self.index + res) % len(self.map)
             self._flag = True
-
 
 App.window = Main
 main = App
