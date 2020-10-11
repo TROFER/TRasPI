efea72ec01a59a8c77a8e2380fb5807a3f9a76ec
TWoolhouse <thomasrwoolhouse@gmail.com>
2020-10-12 00:17:24 +0100

    dummy audio

---
+++ core/driver/dummy/audio.py
@@ -0,0 +1,62 @@
+from . import keys, sinput
+
+class Volume:
+
+    __volume = None
+
+    def __init__(self, sync: int=0):
+        if sync != False or self.__volume is None:
+            self.sync(sync)
+
+    def __constrain(self, val: int, min: int=0, max: int=100):
+        return (min if val < min else (max if val > max else val))
+
+    def __volume_up(self):
+        sinput.key(keys.VOLUME_UP)
+
+    def __volume_down(self):
+        sinput.key(keys.VOLUME_DOWN)
+
+    def set(self, vol: int=0, rel: bool=False):
+        if rel:
+            vol += self.__volume
+        vol = self.__constrain(vol)
+        diff = vol - self.__volume
+        if diff > 0:
+            for i in range(abs(diff // 2)):
+                self.__volume_up()
+        elif diff < 0:
+            for i in range(abs(diff // 2)):
+                self.__volume_down()
+        self.__volume = vol // 2 * 2
+        return self.volume()
+
+    def up(self, amt: int=2):
+        self.set(amt, True)
+    def down(self, amt: int=2):
+        return self.up(-amt)
+
+    def volume(self):
+        return self.__volume
+
+    def sync(self, value: int=0):
+        for i in range(50):
+            self.__volume_down()
+        self.__volume = self.__constrain(value) // 2 * 2
+        for i in range(self.__volume // 2):
+            self.__volume_up()
+
+class media:
+
+    @staticmethod
+    def pause():
+        sinput.key(keys.MEDIA_PLAY_PAUSE)
+    play = pause
+
+    @staticmethod
+    def next():
+        sinput.key(keys.MEDIA_NEXT_TRACK)
+
+    @staticmethod
+    def prev():
+        sinput.key(keys.MEDIA_PREV_TRACK)
---
+++ core/driver/dummy/sinput.py
@@ -0,0 +1,134 @@
+import time
+import ctypes
+import functools as __functools
+import keys
+
+# Import the SendInput object
+SendInput = ctypes.windll.user32.SendInput
+
+# C struct redefinitions
+PUL = ctypes.POINTER(ctypes.c_ulong)
+
+class _KeyboardInput(ctypes.Structure):
+    _fields_ = [
+        ("wVk", ctypes.c_ushort),
+        ("wScan", ctypes.c_ushort),
+        ("dwFlags", ctypes.c_ulong),
+        ("time", ctypes.c_ulong),
+        ("dwExtraInfo", PUL)
+    ]
+
+class _HardwareInput(ctypes.Structure):
+    _fields_ = [
+        ("uMsg", ctypes.c_ulong),
+        ("wParamL", ctypes.c_short),
+        ("wParamH", ctypes.c_ushort)
+    ]
+
+class _MouseInput(ctypes.Structure):
+    _fields_ = [
+        ("dx", ctypes.c_long),
+        ("dy", ctypes.c_long),
+        ("mouseData", ctypes.c_ulong),
+        ("dwFlags", ctypes.c_ulong),
+        ("time", ctypes.c_ulong),
+        ("dwExtraInfo", PUL)
+    ]
+
+class _InputI(ctypes.Union):
+    _fields_ = [
+        ("ki", _KeyboardInput),
+        ("mi", _MouseInput),
+        ("hi", _HardwareInput)
+    ]
+
+class _Input(ctypes.Structure):
+    _fields_ = [
+        ("type", ctypes.c_ulong),
+        ("ii", _InputI)
+    ]
+
+def _send(func):
+    def _send(type, *args, **kwargs):
+        ii = _InputI()
+        func(ii, *args, **kwargs)
+        x = _Input(ctypes.c_ulong(type), ii)
+        SendInput(1, ctypes.pointer(x), ctypes.sizeof(x))
+    return _send
+
+@_send
+def key_pressed(ii, code):
+    ii.ki = _KeyboardInput(code, 0x48, 0, 0, ctypes.pointer(ctypes.c_ulong(0)))
+
+@_send
+def key_released(ii, code):
+    ii.ki = _KeyboardInput(code, 0x48, 0x0002, 0, ctypes.pointer(ctypes.c_ulong(0)))
+
+@_send
+def mouse_pressed(ii, code=keys.LBUTTON):
+    data = 0
+    if code == keys.LBUTTON:
+        flag = 0x0002
+    elif code == keys.RBUTTON:
+        flag = 0x0008
+    elif code == keys.MBUTTON:
+        flag = 0x0020
+    elif code == keys.XBUTTON1:
+        flag = 0x0080
+        data = 0x0001
+    elif code == keys.XBUTTON2:
+        flag = 0x0080
+        data = 0x0002
+    else:
+        flag = 0
+    ii.mi = _MouseInput(0, 0, data, flag, 0, ctypes.pointer(ctypes.c_ulong(0)))
+
+@_send
+def mouse_released(ii, code=keys.LBUTTON):
+    data = 0
+    if code == keys.LBUTTON:
+        flag = 0x0004
+    elif code == keys.RBUTTON:
+        flag = 0x0010
+    elif code == keys.MBUTTON:
+        flag = 0x0040
+    elif code == keys.XBUTTON1:
+        flag = 0x0100
+        data = 0x0001
+    elif code == keys.XBUTTON2:
+        flag = 0x0100
+        data = 0x0002
+    else:
+        flag = 0
+    ii.mi = _MouseInput(0, 0, data, flag, 0, ctypes.pointer(ctypes.c_ulong(0)))
+
+@_send
+def mouse_moved(ii, x, y, rel):
+    ii.mi = _MouseInput(x, y, 0, 0x0001 if rel else 0xC001, 0, ctypes.pointer(ctypes.c_ulong(0)))
+
+def key_press(code):
+    key_pressed(1, code)
+def key_release(code):
+    key_released(1, code)
+
+def click(code, length=0):
+    mouse_pressed(0, code)
+    time.sleep(length)
+    mouse_released(0, code)
+
+def move(x, y, rel=True):
+    mouse_moved(0, x, y, rel)
+
+def key(code, length=0):
+    key_press(code)
+    time.sleep(length)
+    key_release(code)
+
+def modify(key: keys, func):
+    @__functools.wraps(func)
+    def modifier(*args, **kwargs):
+        key_press(key)
+        r = func(*args, **kwargs)
+        key_release(key)
+        return r
+    return modifier
