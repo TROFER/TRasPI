15970b452e7cfa093c41e2d16564699eca63e0a5
TROFER <tristan.day.uk@gmail.com>
2020-10-12 17:44:10 +0100

    Added Litecoin & Created player.py

---
+++ programs/Music Player/window/player.py
@@ -0,0 +1,10 @@
+import core
+from core import Vector
+from core.render.element import Text, Line, Marquee
+
+class Main(core.render.Window):
+
+    def __init__(self, playlist):
+        self.elements = [
+
+        ]
