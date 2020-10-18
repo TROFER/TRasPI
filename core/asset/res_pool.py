0bcc75ff9bd40ebaccb0cc5a1050b0c6cfed73c6
TWoolhouse <thomasrwoolhouse@gmail.com>
2020-10-18 17:52:02 +0100

    Asset Pool can use [key] syntax

--- core/asset/res_pool.py
+++ core/asset/res_pool.py
@@ -12,5 +12,8 @@ class _MetaResourcePool(type):
     def __len__(cls) -> int:
         return sum(True for i in cls.__dict__ if isinstance(i, Asset))
 
+    def __getitem__(cls, key: str) -> Asset:
+        return getattr(cls, key)
+
 class Pool(metaclass=_MetaResourcePool):
     pass
