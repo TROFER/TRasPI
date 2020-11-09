34964f75d96210fc04ebb1d528c2f18d069fd5fc
TWoolhouse <thomasrwoolhouse@gmail.com>
2020-11-09 17:43:06 +0000

    moved player

---
+++ programs/Music Player/player.py
@@ -0,0 +1,3 @@
+import core.driver.player
+
+main = core.driver.player.Player(samplerate=48000, blocksize=8192, buffersize=16)
--- programs/Music Player/player/__init__.py
+++
@@ -1,7 +0,0 @@
-from .Player import Player
-from .Status import Status
-from .Track import Track, TrackBase
-
-__all__ = ["Player", "Track", "Status", "TrackBase"]
-
-main = Player(samplerate=48000, blocksize=8192, buffersize=16)
