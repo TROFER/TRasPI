e8494b367592b9ad1fc3538f22f54e23167b0af5
Tristan <tristan.day.uk@gmail.com>
2021-02-03 00:04:12 +0000

    Int not str on rectangle

--- core/render/element/rectangle.py
+++ core/render/element/rectangle.py
@@ -7,7 +7,7 @@ __all__ = ["Rectangle"]
 
 class Rectangle(Primative):
 
-    def __init__(self, pos1: Vector, pos2: Vector, outline: int=0, fill: str=None, width: int=1, zindex: int=None):
+    def __init__(self, pos1: Vector, pos2: Vector, outline: int=0, fill: int=None, width: int=1, zindex: int=None):
         super().__init__(zindex)
         self.pos1, self.pos2 = pos1, pos2
         self.outline, self.fill, self.width = outline, fill, width
