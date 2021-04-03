25e594f71ed9db51fb7665042b74a66503bec4bd
Tristan <tristan.day.uk@gmail.com>
2021-04-03 20:37:20 +0100

    Renamed import folder to assets | New library 2.0

--- programs/NEA/Final/game/library.py
+++ programs/NEA/Final/game/library.py
@@ -1,168 +1,122 @@
+import json
 import os
 import sqlite3
+import time
 
 from PIL import Image as PIL
 
 
 class Library:
 
-    IMPORT_PATH = f"{core.sys.const.path}programs/NEA/import/"
-    DB_PATH = f"{core.sys.const.path}programs/NEA/resouce/db/assets.db"
-    ASSET_TYPES = ["foreground", "base", "background",
-                   "furniture", "fixing", "palette",
-                   "transition", "player"]
+    Path = {
+        "packages": f"{core.sys.const.path}programs/NEA/assets/",
+        "databases": f"{core.sys.const.path}programs/NEA/resouce/db/"
+    }
 
-    def __init__(self):
-        self.load(self.DB_PATH)
+    def init(self, databases):
+        self.databases = databases
+        for db in self.databases.items():
+            db.db, db.c, rebuild = self.load(db.path)
+            if rebuild and db.loader is not None:
+                db.loader(db)
 
     def load(self, path):
         try:
-            self.db = sqlite3.connect(path)
-            self.c = self.db.cursor()
+            db = sqlite3.connect(path):
+            c = db.cursor()
+            return db, c, False
         except sqlite3.OperationalError:
-            self.build()
-        return self.db, self.c
+            return self.build(path.split("/")[-1].split(".")[0], path)
 
-    def build(self):
-        open(self.DB_PATH, 'wb')
-        self.c.execute("""CREATE TABLE image(
-            id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-            data BLOB,
-            width INTEGER,
-            height INTEGER
-            )""")
-        self.c.execute("""CREATE TABLE type(
-            id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-            name TEXT,
-            end INTEGER
-            )""")
-        self.c.execute("""CREATE TABLE theme(
-            id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-            name TEXT UNIQUE
-            )""")
-        self.c.execute("""CREATE TABLE asset(
-            id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-            type_id INTEGER,
-            theme_id INTEGER,
-            image_id INTEGER,
-            FOREIGN KEY (type_id) REFERENCES  type(id),
-            FOREIGN KEY (theme_id) REFERENCES theme(id),
-            FOREIGN KEY (image_id) REFERENCES image(id)
-            )""")
-        self.c.execute("""CREATE TABLE transition(
-            id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-            center_exit INTEGER,
-            left_exit INTEGER,
-            right_exit INTEGER,
-            FOREIGN KEY (center_exit) REFERENCES image(id),
-            FOREIGN KEY (left_exit) REFERENCES image(id),
-            FOREIGN KEY (right_exit) REFERENCES image(id)
-            )""")
-        self.c.execute("""CREATE TABLE frame(
-            transition_id INTEGER,
-            image_id INTEGER,
-            type TEXT,
-            FOREIGN KEY (transition_id) REFERENCES transition(id),
-            FOREIGN KEY (image_id) REFERENCES image(id)
-            )""")
-        self.c.execute("""CREATE TABLE player_skins(
-            skin id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-            name TEXT,
+    def build(self, name, dest):
+        try:
+            with open(self.Path["databases"] + name, 'r') as file:
+                script = file.read().replace("\n", "").split("%")
+            with open(dest, 'w') as file:
+                file.write("")
+            db, c = self.load(dest)
+            for cmd in script:
+                c.execute(cmd)
+            db.commit()
+            return db, c, True
+        except FileNotFoundError:
+            print("[ERROR] Unable to locate build file")
+
+    def import_asset(self, db, pack_id, asset):
+        image = PIL.open(asset.path).convert("RGBA")
+        db.c.execute("INSERT INTO image (data, width, height) VALUES (?, ?, ?)", [
+            image.tobytes(), image.width, image.height
+        ])
+        image_id = db.c.lastrowid
+        asset_type = asset.name.split(".")[-1].lower()
+        db.c.execute("SELECT id FROM asset-type WHERE name = ?", [asset_type])
+        try:
+            type_id = db.c.fetchone()[0]
+        except TypeError:
+            db.c.execute(
+                "INSERT INTO asset-type (name) VALUES (?)", [asset_type])
+            type_id = db.c.lastrowid
+        db.c.execute("INSERT INTO asset(pack_id, image_id, type_id) VALUES (?, ?, ?)", [
+            pack_id,
             image_id,
-            FOREIGN KEY (image_id) REFERENCES image(id)
-            )""")
-        for _type in self.ASSET_TYPES:
-            self.c.execute(
-                "INSERT INTO type (name, end) VALUES (?, ?)", [_type, False])
-        for _type in self.ASSET_TYPES[:-5]:
-            self.c.execute(
-                "INSERT INTO type (name, end) VALUES (?, ?)", [_type, True])
-        self.load_assets(self.IMPORT_PATH)
-        self.db.commit()
+            type_id
+        ])
 
-    def import_asset(self, path, _type, _theme, end=False):
-        self.c.execute("SELECT id FROM type WHERE name = ? AND end = ?", [
-                       _type, int(end)])
-        type_id = self.c.fetchone()[0]
-        self.c.execute("SELECT id FROM theme WHERE name = ?", [_theme])
-        try:
-            theme_id = self.c.fetchone()[0]
-        except TypeError:
-            self.c.execute("INSERT INTO theme (name) VALUES (?)", [_theme])
-            theme_id = self.c.lastrowid
-        image_id = self.import_image(path)
-        self.c.execute("INSERT INTO asset (type_id, theme_id, image_id) VALUES (?, ?, ?)", [
-            type_id, theme_id, image_id])
-        return self.c.lastrowid
+    def load_assets(self, db):
+        count, start = 0, time.time()
+        for package in os.scandir(f"{core.sys.const.path}programs/NEA/assets/"):
+            try:
+                with open(package.path + "/package.meta") as file:
+                    meta = json.load(file)
+                db.c.execute(
+                    "SELECT id FROM pack-type WHERE name = ?", [meta["type"]])
+                try:
+                    type_id = db.c.fetchone()[0]
+                except TypeError:
+                    db.c.execute(
+                        "INSERT INTO pack-type (name) VALUES (?)", [meta["type"]])
+                    type_id = db.c.lastrowid
+                db.c.execute("INSERT INTO pack (name, type_id, author, size) VALUES (?, ?, ?, ?)", [
+                    package.name,
+                    type_id,
+                    meta["author"],
+                    os.path.getsize(package.path)
+                ])
+                pack_id = db.c.lastrowid
+            except json.JSONDecodeError:
+                print(
+                    f"[ERROR] Failed reading package {package.name} metadata")
+                continue
+            except FileNotFoundError:
+                continue
+            for item in os.scandir(package.path):
+                if item.is_file:
+                    self.import_asset(db, pack_id, item)
+                    count += 1
+            print(f"[INFO] Imported {count} assets in {time.time() - start}s")
 
-    def import_image(self, path):
-        """Returns Bytesdata, Width, Height"""
-        image = PIL.open(path).convert("RGBA")
-        self.c.execute("INSERT INTO image (data, width, height) VALUES (?, ?, ?)", [
-                       image.tobytes(), image.width, image.height])
-        return self.c.lastrowid
-
-    def import_player_skin(self, file):
-        name = file.name.split(".")[0]
-        image_id = self.import_image(file.path)
-        self.c.execute("INSERT INTO player_skins (name, image_id) VALUES (?, ?)", [
-                       name, image_id])
-
-    def load_assets(self, path):
-        _count = 0
-        for _item in os.scandir(path):
-            if "player" in _item.name and _item.is_dir():
-                self.import_player_skin(_item)
-            if "transition" in _item.name and _item.is_dir():
-                for transition in os.scandir(_item.path):
-                    for _file in os.scandir(transition.path):
-                        if "center" in _file.name:
-                            center_exit = self.import_image(_file.path)
-                        elif "left" in _file.name:
-                            left_exit = self.import_image(_file.path)
-                        elif "right" in _file.name:
-                            right_exit = self.import_image(_file.path)
-                    self.c.execute("INSERT INTO transition (center_exit, left_exit, right_exit) VALUES(?, ?, ?)",
-                                   [center_exit, left_exit, right_exit])
-                    transition_id = self.c.lastrowid
-                    for frame in os.scandir(transition.path + "/frames/background"):
-                        image_id = self.import_image(frame.path)
-                        self.c.execute("INSERT INTO frame (transition_id, image_id, type) VALUES (?, ?, ?)", [
-                            transition_id, image_id, "bg"])
-                    for frame in os.scandir(transition.path + "/frames/foreground"):
-                        image_id = self.import_image(frame.path)
-                        self.c.execute("INSERT INTO frame (transition_id, image_id, type) VALUES (?, ?, ?)", [
-                            transition_id, image_id, "fg"])
-            elif _item.is_dir():
-                for _type in os.scandir(f"{self.IMPORT_PATH}{_item.name}"):
-                    if _type.is_dir() and _type.name.lower() in self.ASSET_TYPES:
-                        for _asset in os.scandir(f"{self.IMPORT_PATH}{_item.name}/{_type.name}"):
-                            self.import_asset(
-                                _asset.path, _type.name, _item.name, end=True if "end" in _asset.name else False)
-                            _count += 1
-                    if "palette" in _type.name.lower():
-                        self.import_asset(_type.path, "palette", _item.name)
-        print(f"Assets DB rebuilt. Imported {_count} assets")
-
-    def get_typeid(self, name: str, end: int = 0):
-        self.c.execute(
-            "SELECT id from type WHERE name = ? AND end = ?", [name, end])
-        try:
-            return self.c.fetchone()[0]
-        except TypeError:
-            return None
-
-    def count_records(self, table):
-        self.c.execute(
-            "SELECT count(*) FROM {}".format(table))
-        return self.c.fetchone()[0]
-
-    def fetch_image(self, image_id):
-        self.c.execute(
-            "SELECT data, width, height FROM image WHERE id = ?", [image_id])
-        _image = self.c.fetchone()
-        return PIL.frombytes("RGBA", (_image[1], _image[2]), _image[0])
+    def fetch_image(self, image_id: int):
+        """Returns a PIL image object"""
+        self.databases["assets"].c.execute("""
+        SELECT data, with, height FROM image WHERE id = ?""", [image_id])
+        image = self.databases["assets"].c.fetchone()
+        return PIL.frombytes("RGBA", (image[1], image[2]), image[0])
 
 
-library = Library()
-library.build()
+class Database:
+
+    def __init__(self, path, loader=None):
+        self.path = path
+        self.loader = loader
+        self.db = None
+        self.c = None
+
+
+lib = Library()
+
+databases = {
+    "assets": Database(
+        f"{core.sys.const.path}programs/NEA/resouce/db/assets.db", lib.load_assets)
+}
+
+lib.init(databases)
