70a8735decf9e77eca5485dc44eb2b65d71ef539
Tristan <tristan.day.uk@gmail.com>
2021-04-22 22:16:21 +0100

    Fixed Keybindings Being Overridden Error

--- core/driver/keyboard/keyboard.py
+++ core/driver/keyboard/keyboard.py
@@ -19,8 +19,12 @@ _bindings = {
 }
 
 def bind(button, function):
-    #print(_map[button], function)
-    if _bindings[button] is not None:
-        keyboard.remove_hotkey(_bindings[button])
-    _bindings[button] = keyboard.add_hotkey(
+    try:
+        if _bindings[button] is not None:
+            keyboard.remove_hotkey(_bindings[button])
+        _bindings[button] = keyboard.add_hotkey(
         _map[button], function, args=(button, "press"), trigger_on_release=False)
+    except ValueError:
+        pass
+    except KeyError:
+        pass
